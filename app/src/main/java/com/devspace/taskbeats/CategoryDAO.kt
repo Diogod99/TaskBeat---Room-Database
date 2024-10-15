85d13bc434f811d2068df5cd4b7f53adec7366e1
Diogo <99diogod@gmail.com>
2024-10-15 16:41:09 +0100

    Creating Room Database

--- app/src/main/java/com/devspace/taskbeats/CategoryDAO.kt
+++ app/src/main/java/com/devspace/taskbeats/CategoryDAO.kt
@@ -2,15 +2,17 @@ package com.devspace.taskbeats
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 
 @Dao
 interface CategoryDAO {
+
     @Query("Select * From categoryentity")
     fun getAll(): List<CategoryEntity>
 
-    @Insert
-    fun insertAll (vararg categoryEntity: CategoryEntity)
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll (categoryEntity: List<CategoryEntity>)
 
 
 }
--- app/src/main/java/com/devspace/taskbeats/MainActivity.kt
+++ app/src/main/java/com/devspace/taskbeats/MainActivity.kt
@@ -2,10 +2,26 @@ package com.devspace.taskbeats
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.room.Room
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    private val db by lazy {
+        Room.databaseBuilder(
+            applicationContext,
+            TaskBeatDataBase::class.java, "database-task-beat"
+        ).build()
+    }
+
+    private val categoryDao by lazy {
+        db.getCategoryDAO()
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -42,6 +58,20 @@ class MainActivity : AppCompatActivity() {
         rvTask.adapter = taskAdapter
         taskAdapter.submitList(tasks)
     }
+
+    private fun insertDefaultCategory() {
+        val categoriesEntity = categories.map {
+            CategoryEntity(
+                name = it.name,
+                isSelected = it.isSelected
+            )
+        }
+
+        GlobalScope.launch(Dispatchers.IO) {
+            categoryDao.insertAll(categoriesEntity)
+        }
+    }
+
 }
 
 val categories = listOf(
